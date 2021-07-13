7afd0574818d16d84025897a28dd2e2de59e2085
adam <adam.lee346@gmail.com>
2021-07-13 15:45:32 -0600

    Add one_to_many association users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
         respond_to do |format|
             if @article.save
             @article.save
