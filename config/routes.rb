0bae1c6fc26c6f5ddcf1967005913835e04bebb3
adam <adam.lee346@gmail.com>
2021-07-14 10:01:23 -0600

    user controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
-  get 'topstories', to: 'pages#topstories'
+
   resources :articles
+  resources :users, except: [:new]
+
+  get 'topstories', to: 'pages#topstories'
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
 end  
