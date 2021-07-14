edfe71a45322b2c608c83846d7a807f5c16ac841
adam <adam.lee346@gmail.com>
2021-07-14 15:12:57 -0600

    added login form

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
 
   get 'topstories', to: 'pages#topstories'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # post 'users', to: 'users#create'
 end  
