dc5237c9c1767f1274f2b16ae32682ccff9f1fe8
adam <adam.lee346@gmail.com>
2021-07-14 11:04:06 -0600

    creating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
     def new
         @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+       if @user.save
+            flash[:notice] = "Welcome to Spacepress!, sign up successful." 
+            redirect_to users_path
+       else
+            render 'new'
+       end 
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
