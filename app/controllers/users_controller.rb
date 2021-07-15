b81e52181337e0616171834fb04764cea23f887a
adam <adam.lee346@gmail.com>
2021-07-15 14:37:13 -0600

    added helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+
     def index
         @users = User.all
     end
@@ -7,15 +9,14 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id]) 
+        
     end 
 
     def edit
-        @user = User.find(params[:id])
+        
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Account Successfully updated"
             redirect_to articles_path
@@ -26,16 +27,21 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-       if @user.save
-            flash[:notice] = "Welcome to Spacepress!, sign up successful." 
+        if @user.save
+            session[:user_id] = @user.id
+            flash[:notice] = "Welcome to Spacepress!, #{@user.username} sign up successful." 
             redirect_to users_path
-       else
+        else
             render 'new'
-       end 
+        end 
     end
 
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
